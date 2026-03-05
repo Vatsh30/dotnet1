de3b08f0846fc30b47b939c98b203cfdea692754
Vatsh Patel <24amtics086@gmail.com>
2026-03-05 14:38:52 +0530

    Create TodoItem.cs

---
+++ TodoItem.cs
@@ -0,0 +1,11 @@
+namespace TodoApiJson.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; }
+    }
+}
